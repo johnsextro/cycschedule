3736fdeef663f303abee242ffe13d2fb4b68694f
John Sextro <john.sextro@gmail.com>
2015-06-14 13:07:41 -0500

    Now loading seasons from web service. Allowing rows to be selected.

--- cycschedule/SeasonsViewController.swift
+++ cycschedule/SeasonsViewController.swift
@@ -2,11 +2,11 @@ import Foundation
 import UIKit
 
 var postEndpoint: String = "http://x8-avian-bricolage-r.appspot.com/season/SeasonService.season"
+var TableData:Array< String > = Array < String >()
+var lastSelectedIndexPath: NSIndexPath?
 
 class SeasonsViewController: UITableViewController {
     
-    var seasons: [String] = []
-
     override func viewDidLoad() {
         let timeout = 15
         let url = NSURL(string: postEndpoint)
@@ -22,8 +22,7 @@ class SeasonsViewController: UITableViewController {
                 error: NSError!) in
                 if data.length > 0 && error == nil{
                     let json = NSString(data: data, encoding: NSASCIIStringEncoding)
-//                    self.extract_json(json!)
-                    println(json)
+                    self.extract_json(json!)
                 }else if data.length == 0 && error == nil{
                     println("Nothing was downloaded")
                 } else if error != nil{
@@ -38,15 +37,65 @@ class SeasonsViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return seasons.count
+        return TableData.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("TeamCell", forIndexPath: indexPath) as! UITableViewCell
-        
-        let season = seasons[indexPath.row] as String
-        
-        cell.textLabel?.text = "Season"
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
+        cell.accessoryType = .None
+        cell.textLabel?.text = TableData[indexPath.row]
         return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+
+        
+        if indexPath.row != lastSelectedIndexPath?.row {
+            if let lastSelectedIndexPath = lastSelectedIndexPath{
+                let oldCell = tableView.cellForRowAtIndexPath(lastSelectedIndexPath)
+                oldCell?.accessoryType = .None
+            }
+            
+            let newCell = tableView.cellForRowAtIndexPath(indexPath)
+            newCell?.accessoryType = .Checkmark
+            
+            lastSelectedIndexPath = indexPath
+        }
+    }
+    
+    func extract_json(data:NSString)
+    {
+        var parseError: NSError?
+        let jsonData:NSData = data.dataUsingEncoding(NSASCIIStringEncoding)!
+        let json: AnyObject? = NSJSONSerialization.JSONObjectWithData(jsonData, options: nil, error: &parseError)
+        if (parseError == nil)
+        {
+            if let seasons_obj = json as? NSDictionary
+            {
+                if let seasons = seasons_obj["seasons"] as? NSArray
+                {
+                    for (var i = 0; i < seasons.count ; i++ )
+                    {
+                        if let season_obj = seasons[i] as? NSDictionary
+                        {
+                            if let season = season_obj["season"] as? String
+                            {
+                                TableData.append(season)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        do_table_refresh();
+    }
+    
+    func do_table_refresh()
+    {
+        dispatch_async(dispatch_get_main_queue(), {
+            self.tableView.reloadData()
+            return
+        })
     }
 }
